145a842f5af57bf6533e0bc9aaefbb0ba95de1f1
tom.vaughan <tom_vaughan@hotmail.com>
2012-02-05 18:46:39 -0500

    Adding main() method handling for taking a "-board 'X O X _ X _ O _ X'" style command line parameter or a "-file foo.txt" style parameter
    The -board option is supported as of this commit (tested via my NetBeans "run with" build properties), but the "-file" option is just stubbed out to return an empty board

--- ch05_scala/day1-tic-tac-toe/src/main/scala/com/opower/TicTacToeChecker.scala
+++ ch05_scala/day1-tic-tac-toe/src/main/scala/com/opower/TicTacToeChecker.scala
@@ -3,13 +3,64 @@ package com.opower
 import com.opower.ttt._
 
 /**
- * Hello world!
+ * "Driver" class for reading in the state of a tic-tac-toe board and making a determination of whether or not any player
+ * has won.
+ * 
+ * Usage:
+ *   1) TicTacToeChecker -file <filename>
+ *      where <filename> points to an ASCII text file containing 3 rows of 3 characters each of (X, O, or _)
+ *      eg:
+ *         X O _
+ *         _ _ O
+ *         X X X
+ *         
+ *   2) TicTacTorChecker -board <board pattern>
+ *      where <boardpattern> is a quoted string of space-delimited "X", "O" or "_" characters
+ *      eg:
+ *         -board "X O _ _ _ O X X X"
  *
  */
 object TicTacToeChecker {
 
   def main(args: Array[String]) {
-    val board = new Board(Array.fill(3,3)(CellState.BLANK))
-    println("*****\n" + board + "\n*******")
+    var boardStrings: Array[String] = null
+    
+    if(args != null && args.nonEmpty) {
+      if(args(0).equals("-file")) {
+        require(args(1) != null)
+        boardStrings = parseBoardStringsFromFile(args(1))
+      }
+      else if(args(0).equals("-board")) {
+        require(args(1) != null)
+        boardStrings = args(1).split("[ ]")
+      }
+      else {
+        println("Unknown initial argument " + args(0))
+        exit(0)
+      }
+    } else {
+      println("No [-file <file>] or [-board <X _ O ...>] input provided, assuming blank board")
+      boardStrings = Array("_", "_", "_", "_", "_", "_", "_", "_", "_")
+    }
+    
+    val board = Board.fromStrings(boardStrings:_*)
+    
+    println("Using this board: ")
+    println("-------------------------------------------")
+    println(board)
+    println("-------------------------------------------")
+    
+    if(board.anyBoardWin(CellState.X)) {
+      println("\n***\tPlayer X has won!\n\n")
+    }
+    else if(board.anyBoardWin(CellState.O)) {
+      println("\n***\tPlayer O has won!\n\n")
+    }
+    
+  }
+  
+  def parseBoardStringsFromFile(filename : String) : Array[String] = {
+    // TODO complete this
+    Array("_", "_", "_", "_", "_", "_", "_", "_", "_")
   }
 }
