54845df4fc8a1738590713fc088b2b0ce6fd5a35
tom.vaughan <tom_vaughan@hotmail.com>
2012-02-05 11:44:26 -0500

    Renaming BoardTest to BoardSuite and adopting the scalatest framework instead of the Junit framework (well, it's not really "instead of", but the style is different enough I'm thinking of it as a different beast)

---
+++ ch05_scala/day1-tic-tac-toe/src/test/scala/com/opower/ttt/BoardSuite.scala
@@ -0,0 +1,38 @@
+package com.opower.ttt
+
+import org.scalatest.junit.JUnitSuite
+import org.scalatest.junit.ShouldMatchersForJUnit
+import scala.collection.mutable.ListBuffer
+import org.junit.Test
+import org.junit.Before
+import org.junit.Assert._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+
+@RunWith(classOf[JUnitRunner])
+class BoardSuite extends FunSuite {
+  
+  test("happy path construction") {
+    val board = new Board(Array.fill(3,3)(" "))
+    assertNotNull(board)
+    assertNotNull(board.toString)
+
+    println(board.toString)
+  }
+  
+  
+  // A tic-tac-toe board needs to be a 3x3 grid
+  test("construction should fail when first array index isn't == 3") {
+    intercept[IllegalArgumentException] {
+      new Board(Array.fill(1, 3)(" "))
+    }
+  }
+  
+  // A tic-tac-toe board needs to be a 3x3 grid
+  test("construction should fail when second array index isn't == 3") {
+    intercept[IllegalArgumentException] {
+      new Board(Array.fill(3, 99)(" "))
+    }
+  }
+}
