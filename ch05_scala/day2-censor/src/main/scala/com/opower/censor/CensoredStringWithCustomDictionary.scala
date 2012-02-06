7d1cf4c0d7a5b45948bf5ec3f2033dbea3c31a9f
tom.vaughan <tom_vaughan@hotmail.com>
2012-02-06 15:48:30 -0500

    Adding the implementation of a custom dictionary fetching class that overrides the default dictionary provided by the trait itself

--- ch05_scala/day2-censor/src/main/scala/com/opower/censor/CensoredStringWithCustomDictionary.scala
+++ ch05_scala/day2-censor/src/main/scala/com/opower/censor/CensoredStringWithCustomDictionary.scala
@@ -1,16 +1,24 @@
 package com.opower.censor
 
+import scala.io.Source
+
 /**
  * Just like CensoredString, but provides its own dictionary to do the curse-word-filtering
  */
 class CensoredStringWithCustomDictionary(override val content:String) extends RawString(content) with Censor {
   
+  private val censorshipDictionaryFile: String = "custom_dictionary.txt"
 
-
-  val myDictionary = Map("fucked" -> "jacked",
-                              "shitty" -> "rotten",
-                              "nasty"  -> "Yoni's Mom-ish")
-  
-  override def dictionary() : Map[String, String] = { myDictionary }  
+  override def dictionary() : Map[String, String] = { 
+    val myDictionary = scala.collection.mutable.Map[String, String]()
+    
+    val dictList = Source.fromFile(censorshipDictionaryFile).getLines.toList
+    dictList.foreach{(line) => 
+      val entries: Array[String] = line.split(" => ")
+      myDictionary.put(entries(0).trim, entries(1).trim)
+    }
+    
+    myDictionary.toMap     // .toMap to convert from mutable (needed during construction) to immutable
+  }  
 
 }
