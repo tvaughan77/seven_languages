5b1522ca3c9706238b241a066da381206043cbb5
tom.vaughan <tom_vaughan@hotmail.com>
2012-02-06 15:15:59 -0500

    Javadoc

--- ch05_scala/day2-censor/src/main/scala/com/opower/censor/CensoredString.scala
+++ ch05_scala/day2-censor/src/main/scala/com/opower/censor/CensoredString.scala
@@ -1,3 +1,10 @@
 package com.opower.censor
 
+/**
+ * A class that extends the "RawString" class (which you can think about as the java.lang.String surrounded by a wrapper
+ * class that returns you the String with a call to .getContent()
+ * 
+ * This class also uses the "Censor" mixin trait which intercepts any call to getContent() and replaces curse words with
+ * tamer alternatives.
+ */
 class CensoredString(override val content:String) extends RawString(content) with Censor 
