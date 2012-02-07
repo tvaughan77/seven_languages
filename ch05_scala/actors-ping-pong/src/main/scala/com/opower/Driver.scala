76dbc6a1c76b8bd3bb4a31fdcfc87d0464fb5e58
tom.vaughan <tom_vaughan@hotmail.com>
2012-02-07 16:22:36 -0500

    Adding "react" versions of the Ping and Pong classes.

--- ch05_scala/actors-ping-pong/src/main/scala/com/opower/Driver.scala
+++ ch05_scala/actors-ping-pong/src/main/scala/com/opower/Driver.scala
@@ -1,18 +1,29 @@
 package com.opower
 
-import com.opower.pingpong._
-
 object Driver {
   def main(args: Array[String]) {
     /*
      * This version will use the actor "receive" method that involves Thread blocking
      */
-    playPingPong_With_Receive()
+//    playPingPong_With_Receive()
+    
+    /*
+     * This version will use the actor "react" method that maximizes thread pool reuse
+     */
+    playPingPong_With_React()
   }
   
   def playPingPong_With_Receive() {
-    import com.opower.pingpong.Stop
     import com.opower.pingpong.receive._
+    
+    val pong = new Pong
+    val ping = new Ping(100000, pong)
+    ping.start
+    pong.start
+  }
+  
+  def playPingPong_With_React() {
+    import com.opower.pingpong.react._
     
     val pong = new Pong
     val ping = new Ping(100000, pong)
---
+++ ch05_scala/actors-ping-pong/src/main/scala/com/opower/pingpong/react/Ping.scala
@@ -0,0 +1,38 @@
+package com.opower.pingpong.react
+
+import scala.actors.Actor
+import scala.actors.Actor._
+import com.opower.pingpong.Stop
+
+case object Ping
+
+class Ping(count: Int, pong: Actor) extends Actor {
+  def act() {
+    
+    var pingsLeft = count - 1
+    pong ! Ping
+    
+    loop {
+    
+      react {
+      
+        case Pong =>
+        
+          if (pingsLeft % 1000 == 0)
+            println("Ping: pong")
+          
+          if (pingsLeft > 0) {
+            pong ! Ping
+            pingsLeft -= 1
+          
+          } else {
+            println("Ping: stop")
+            pong ! Stop
+            exit()
+          }
+    
+      }
+    
+    }
+  }
+}
